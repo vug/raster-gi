53f4f32a1de457ef1169eb3c1368249b0ae5beea
Ugur Guney <ugurguney@gmail.com>
2024-10-15 23:45:06 -0400

    some references

--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@
 * TODO(vug): New opengl.hpp file with OpenGL types. put in gl namespace.
 * TODO(vug): Either remove the double window/context creation path for modern pixel buffer choice or make it optional
 * TODO(vug): Better function pointer naming scheme than PFNGLCLEARCOLORPROC
+* ref: 
+* https://www.khronos.org/opengl/wiki/Platform_specifics:_Windows
+* https://gist.github.com/nickrolfe/1127313ed1dbf80254b614a721b3ee9c
+* https://www.khronos.org/opengl/wiki/Creating_an_OpenGL_Context_(WGL)
+* https://glad.dav1d.de/
 */
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
@@ -110,6 +115,7 @@ void loadWglCreateContextAttribsARB() {
   DestroyWindow(dummyWindowHandle);
 }
 
+// from: https://www.khronos.org/opengl/wiki/Load_OpenGL_Functions
 // Functions belonging to OpenGL v1.1 or less cannot be loaded via wglGetProcAddress
 // we'll get them directly from opengl32.dll that comes with Windows
 void *GetAnyGLFuncAddress(const char *name) {
